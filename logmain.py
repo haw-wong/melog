a62315bf927312d6010d83ef59fc63aee3c9fc03
Haw Wong <63915576+haw-wong@users.noreply.github.com>
2026-03-30 15:51:16 +0800

    Add files via upload

--- logmain.py
+++ logmain.py
@@ -19,12 +19,13 @@ def init_page(title, layout, icon):
     return
 
 def get_shipdata(sheet):
-    with xw.App(visible=False) as app:
-        wb = xw.Book('mydata.xlsx')
-        sheet = wb.sheets['ship']
-        dfShip = sheet['A1'].options(pd.DataFrame,index=False,expand='table').value  
-        wb.close()
-        app.quit()
+    #with xw.App(visible=False) as app:
+    app = xw.App(visible=False)
+    wb = xw.Book('mydata.xlsx')
+    sheet = wb.sheets['ship']
+    dfShip = sheet['A1'].options(pd.DataFrame,index=False,expand='table').value  
+    wb.close()
+    #app.quit()
     return dfShip
 
 
